a2524961da00cff585675f3624d9f271c6f26fcb
Andri Thorlacius <andri.thorlacius@gmail.com>
2015-10-27 16:18:06 +0000

    Updated the job and list endpoint to return correct data and added docblocks

--- src/Zenter/Api/v1/JobEndpoint.php
+++ src/Zenter/Api/v1/JobEndpoint.php
@@ -15,40 +15,83 @@ namespace Zenter\Api\v1
 			$this->restClient = $restClient;
 		}
 
+		/**
+		 * Get a list of all jobs
+		 *
+		 * @return array|null
+		 * @throws \Exception
+		 */
 		public function GetAll()
 		{
 			$action = '/jobs';
-			$this->restClient->call($action);
+			$data = $this->restClient->call($action);
+
+			return Helper::JsonToArray($data);
 		}
 
+		/**
+		 * Get a job by id
+		 *
+		 * @param int $id
+		 *
+		 * @return null|object
+		 * @throws \Exception
+		 */
 		public function GetById($id)
 		{
 			$action = '/jobs/email/' . $id;
 			$job = $this->restClient->call($action);
 
-			return $job;
+			return Helper::JsonToObject($job);
 		}
 
-		public function CreateJob($data)
+		/**
+		 * Create a job
+		 *
+		 * @param array $data
+		 *
+		 * @return null|object
+		 * @throws \Exception
+		 */
+		public function CreateJob(array $data)
 		{
 			$action = '/jobs/email/';
 			$response = $this->restClient->call($action, $data, 'POST');
 
-			return $response;
+			return Helper::JsonToObject($response);
 		}
 
-		public function UpdateById($id, $data)
+		/**
+		 * Update job by id
+		 *
+		 * @param int   $id
+		 * @param array $data
+		 *
+		 * @return null|object
+		 * @throws \Exception
+		 */
+		public function UpdateById($id, array $data)
 		{
 			$action = '/jobs/email/' . $id;
 			$response = $this->restClient->call($action, $data, 'POST');
 
-			return $response;
+			return Helper::JsonToObject($response);
 		}
 
+		/**
+		 * Send a Job
+		 * This will trigger the job to start sending
+		 *
+		 * @param int $id
+		 *
+		 * @return bool
+		 */
 		public function SendJob($id)
 		{
 			$action = '/jobs/email/' . $id . '/send';
 			$this->restClient->call($action);
+
+			return $this->restClient->GetStatusCode() === 200;
 		}
 	}
 }
--- src/Zenter/Api/v1/ListEndpoint.php
+++ src/Zenter/Api/v1/ListEndpoint.php
@@ -10,15 +10,28 @@ namespace Zenter\Api\v1
 		 */
 		private $restClient;
 
+		/**
+		 * @param IHttpClient $restClient
+		 */
 		public function __construct(IHttpClient $restClient)
 		{
 			$this->restClient = $restClient;
 		}
 
+		/**
+		 * Add a recipient to a list
+		 *
+		 * @param int $listId
+		 * @param int $recipientId
+		 *
+		 * @return bool
+		 */
 		public function AddRecipient($listId, $recipientId)
 		{
 			$action = "/lists/" . $listId . "/recipients/add/" . $recipientId;
 			$this->restClient->call($action);
+
+			return $this->restClient->GetStatusCode() === 200;
 		}
 	}
 }
